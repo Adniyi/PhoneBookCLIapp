362dbcf43cabcffb3dcea26a44ecad4742d383d0
David <davidkehinde145@gmail.com>
2024-11-18 22:31:47 -0800

    Phone Book CLI app

--- obj/Debug/net8.0/Phone Book Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/Phone Book Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Phone Book Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a10b4fc9938fb1287f64e163fdaa0e4429e175")]
 [assembly: System.Reflection.AssemblyProductAttribute("Phone Book Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Phone Book Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
